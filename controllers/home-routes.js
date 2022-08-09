f879a1b9b215b5797f432d137c922227b6fb8347
Zach Fulmer <Zachfulmer@earthlink.net>
2022-08-09 10:54:03 -0500

    fixed merge issues

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection.js");
-const { Product} = require("../models");
+const {Product} = require("../models");
 
 router.get("/", (req, res) => {
   Product.findAll()
