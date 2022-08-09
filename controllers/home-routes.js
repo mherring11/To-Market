3d95b6851bfa5e56336805a1134ef062e854decb
FPCITS <sara@freedompracticecoaching.com>
2022-08-08 19:33:46 -0500

    moved assets to public folder

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,40 +1,42 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection.js');
+const router = require("express").Router();
+const sequelize = require("../config/connection.js");
+const { Product, User } = require("../models");
 
-router.get('/', (req, res) => {
-    Product.findAll().then(dbInventoryData => {
-    const items = dbInventoryData.map(item => item.get({ plain: true }));
+router.get("/", (req, res) => {
+  Product.findAll()
+    .then((dbInventoryData) => {
+      const items = dbInventoryData.map((item) => item.get({ plain: true }));
 
-    res.render('homepage', {
+      res.render("homepage", {
         items,
-        title: 'To-Market',
-        loggedIn: req.session.loggedIn
-    });
+        title: "To-Market",
+        loggedIn: req.session.loggedIn,
+      });
     })
-    .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/dashboard');
-        return;
-    }
-    res.render('login', {
-        title: 'To-Market'
-    });
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/dashboard");
+    return;
+  }
+  res.render("login", {
+    title: "To-Market",
+  });
 });
 
-router.get('/signup', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/dashboard');
-        return;
-    }
-    res.render('signup', {
-        title: 'To-Market'
-    });
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/dashboard");
+    return;
+  }
+  res.render("signup", {
+    title: "To-Market",
+  });
 });
 
 module.exports = router;
